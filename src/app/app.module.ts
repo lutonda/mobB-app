e22b49a291ef963a2c12045d73de3411b82175c7
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2019-09-30 15:52:09 +0000

    currecny components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrencyComponent } from './components/currency/currency.component';
+import { StartComponent } from './components/start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
