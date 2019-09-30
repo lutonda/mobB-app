553d5397dbaf150e0527b87f47877f89a2c0f0a2
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2019-09-30 16:10:28 +0000

    componetization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CurrencyComponent } from './components/currency/currency.component';
+import { StartComponent } from './components/start/start.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: StartComponent },
+  { path: 'currency', component: CurrencyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
